6411de18d6942f3255cdd04cf3e47b45591d6947
John Cornia <john.cornia.1@gmail.com>
2021-11-08 08:30:46 -0700

    styling and js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-import { hideDivElement } from "./hide.js";
+function generateWOD(formData) {
+    let myString = "<tr><th>Exercise</th><th>Sets</th><th>Repetitions</th><tr>";
+    
+    document.getElementById("WOD").innerHTML = myString;
+}
 
-document.getElementById("trainingDay").addEventListener("click", hideDivElement());
+document.getElementById("workoutForm").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
+  document.getElementById("submisssion").addEventListener("click", function () {
+    let myForm = document.getElementById('workoutForm');
+    let formData = new FormData(myForm);
+    for (var key of formData.keys()) {
+        console.log(formData.get(key));
+     }
+     generateWOD(formData);
+  })
