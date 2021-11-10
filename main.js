2519d24fab0cb6be9e1e630864c5ac2d497e68b5
John Cornia <john.cornia.1@gmail.com>
2021-11-10 15:38:08 -0700

    inputs and labels now hidden before previous input is done

--- main.js
+++ main.js
@@ -8,11 +8,46 @@ function generateWOD(formData) {
     document.getElementById("WOD").innerHTML = myString;
 }
 
-document.getElementById("workoutForm").addEventListener("click", function(event){
+document.getElementById("workoutForm").addEventListener("click", function (event) {
     event.preventDefault()
-  });
+});
 
-  document.getElementById("submisssion").addEventListener("click", function () {
+document.getElementById("week").addEventListener("change", function () {
+    let training = document.getElementsByClassName("training");
+    training[0].style.display = "block";
+    training[1].style.display = "block";
+})
+
+document.getElementById("trainingDay").addEventListener("change", function () {
+    let previously = document.getElementsByClassName("previously");
+    let proximately = document.getElementsByClassName("proximately");
+    let training = document.getElementById("trainingDay");
+    let trainingSelected = training.options[training.selectedIndex].value;
+
+    if (trainingSelected == "dynamic") {
+        previously[0].style.display = "block";
+        previously[1].style.display = "block";
+        proximately[0].style.display = "none";
+    }
+    else{
+        proximately[0].style.display = "block";
+        proximately[1].style.display = "block"; 
+        previously[0].style.display = "none";
+    }
+
+})
+
+document.getElementById("lift").addEventListener("blur", function () {
+    let lift = document.getElementById("lift").value;
+
+    if (lift != "") {
+        let record = document.getElementsByClassName("record");
+        record[0].style.display = "block";
+        record[1].style.display = "block";
+    }
+})
+
+document.getElementById("submisssion").addEventListener("click", function () {
     let myForm = document.getElementById('workoutForm');
     let formData = new FormData(myForm);
     //console.log(formData.get(" week"));
@@ -20,5 +55,5 @@ document.getElementById("workoutForm").addEventListener("click", function(event)
         console.log(formData.get(key));
         console.log(key);
      }*/
-     generateWOD(formData);
-  })
+    generateWOD(formData);
+})
